fcd2556204e8c656e1e3f159bee3710317add79e
Alaa Masoud <alaa.masoud@live.com>
2021-12-11 01:25:33 +0400

    trigger workflow

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Diagnostics;
+#if WINDOWS
+using System.Management;
+#endif
+
+if (Proc.IsAzuriteRunning())
+{
+    Console.WriteLine("Running.");
+}
+else
+{
+    Console.WriteLine("Not Running.");
+}
+
+public static class Proc
+{
+    public static bool IsAzuriteRunning()
+    {
+        foreach (var p in Process.GetProcessesByName("node"))
+        {
+            if (p.GetCommandLine().Contains("azurite", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static string GetCommandLine(this Process process)
+    {
+        try
+        {
+#if WINDOWS
+#pragma warning disable CA1416 // Validate platform compatibility
+            using var searcher = new ManagementObjectSearcher($"SELECT CommandLine FROM Win32_Process WHERE ProcessId = {process.Id}");
+            using var objects = searcher.Get();
+            return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString() ?? "";
+#pragma warning restore CA1416 // Validate platform compatibility
+#elif LINUX
+            return File.ReadAllText($"/proc/{process.Id}/cmdline");
+#else
+            throw new PlatformNotSupportedException();
+#endif
+        }
+        catch
+        {
+            return string.Empty;
+        }
+    }
+}
